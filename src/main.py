bbe8ed7047fdf8163914ecf06ba6ecf9a42f546c
José Augusto <josemoura212@gmail.com>
2024-05-09 16:49:32 -0300

    chore: Refactor chat_model function to handle special inputs

--- src/main.py
+++ src/main.py
@@ -2,13 +2,16 @@ from config import init_gemini
 from ui import menu_options
 from models import chat_model
 
+def main():
+    model = init_gemini()
 
-model = init_gemini()
+    chat = model.start_chat(history=[])
 
-chat = model.start_chat(history=[])
+    options = menu_options()
 
-options = menu_options()
+    match options:
+        case "1":
+            chat_model(chat)
 
-match options:
-    case "1":
-        chat_model(chat)
+if __name__ == '__main__':
+    main()
--- src/models.py
+++ src/models.py
@@ -1,11 +1,21 @@
 from termcolor import colored
+from google.generativeai import ChatSession
 
-def chat_model(chat_model):
+special_input = ["sair","arquivo","audio"]
+
+def chat_model(chat_model:ChatSession):
     while True:
         user_input = input(colored("Pergunta: ","blue"))
-        if user_input == "sair":
-            print(colored("\nBot: Até mais!","red"))
-            break
+        if user_input in special_input:
+            if user_input.lower() == "sair":
+                print(colored("\nBot: Até mais!","red"))
+                break
+            elif user_input.lower() == "audio":
+                print(colored("\nBot: Processando...","green"))
+                
+                chat_model.send_message(user_input,content="audio")
+        else:
+            continue
         print("\nSua Pergunta:",user_input)
         print(colored("\nBot: Processando...","green"))
         response = chat_model.send_message(user_input)
