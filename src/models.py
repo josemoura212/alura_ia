4773f25cc1094a8a575720325a2d2b301fcf6b80
José Augusto <josemoura212@gmail.com>
2024-05-09 17:02:20 -0300

    feat: Update chat_model function to handle special inputs

--- src/models.py
+++ src/models.py
@@ -6,16 +6,9 @@ special_input = ["sair","arquivo","audio"]
 def chat_model(chat_model:ChatSession):
     while True:
         user_input = input(colored("Pergunta: ","blue"))
-        if user_input in special_input:
-            if user_input.lower() == "sair":
-                print(colored("\nBot: Até mais!","red"))
-                break
-            elif user_input.lower() == "audio":
-                print(colored("\nBot: Processando...","green"))
-                
-                chat_model.send_message(user_input,content="audio")
-        else:
-            continue
+        if user_input.lower() == "sair":
+            print(colored("\nBot: Até mais!","red"))
+            break
         print("\nSua Pergunta:",user_input)
         print(colored("\nBot: Processando...","green"))
         response = chat_model.send_message(user_input)
